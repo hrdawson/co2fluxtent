2742d4d14de97eb31cddceccc50191557352027e
hrdawson <36278527+hrdawson@users.noreply.github.com>
2023-12-06 08:57:54 +0200

    Added comments

--- R/read_files.R
+++ R/read_files.R
@@ -43,23 +43,22 @@
 #'
 #' @export
 read_files <- function(path, photo = "photo", resp = "resp", ambient = "a") {
-
+# Write error messages
   if (missing(path)) {
  readline("Please set the working directory to the folder\n that contains the LiCOR files to be analyzed.\n Do so with the upcoming prompt. Note that you must choose a file in the folder that\n you want to set as the working directory. \nPlease press 'return' to continue.")
     files <- list.files(dirname(file.choose()), full.names = TRUE)
   } else {
     files <- list.files(path, full.names = TRUE)
   }
-
+#List names of photosynthesis fluxes
   photo.names <- grep(paste0("[^", resp, "].txt$"),
                       grep(paste0("[^_", ambient, "]\\.txt$"),
                            files, value = TRUE), value = TRUE)
-  # Identify ambient flux measurement files
+  # #List names of ambient fluxes
   # ambient.names <- grep(paste0(ambient, ".txt"), files, value = TRUE)
   ambient_pattern <- paste0(ambient, ".txt$")
   ambient.names <- grep(ambient_pattern, files, value = TRUE)
-
-  # Identify respiration measurement files
+#List names of respiration fluxes
   # resp.names <- grep(paste0(resp, ".txt"), files, value = TRUE)
   respiration_pattern <- paste0(resp, ".txt$")
   resp.names <- grep(respiration_pattern, files, value = TRUE)
@@ -74,7 +73,7 @@ read_files <- function(path, photo = "photo", resp = "resp", ambient = "a") {
     message("No matching ambient files found. First second of photo files will be used.")
   }
 
-
+# Make an object with all the file names
   licor_files <- list(photo_names = photo.names,
                       ambient_names = ambient.names,
                       resp_names = resp.names)
